b6612c1705ad88a623e23533f9e46d4d49221c25
Andreas Wilke <andiwilke@gmail.com>
2024-02-01 19:57:11 +0100

    test

--- outputs.tf
+++ outputs.tf
@@ -6,25 +6,3 @@ output "resource_group_name" {
   value = var.resource_group_name
 }
 
-output "vnet_id" {
-  value = azurerm_virtual_network.wiz_aks.id
-}
-
-output "vnet_cidr" {
-  value = azurerm_virtual_network.wiz_aks.address_space[0]
-}
-
-output "subnet_id" {
-  description = "Bring-Your-Own-Network subnet ID"
-  value       = azurerm_subnet.wiz_aks_subnet.id
-}
-
-output "proxy_private_ip" {
-  description = "Proxy server private IP"
-  value       = local.proxy_private_ip
-}
-
-output "proxy_ip" {
-  description = "Proxy server public IP"
-  value       = local.proxy_public_ip
-}
--- variables.tf
+++ variables.tf
@@ -15,44 +15,8 @@ variable "resource_group_region" {
   type = string
 }
 
-variable "vnet_name" {
-  type    = string
-  default = "wiz-vnet"
-}
-
-variable "vnet_cidr" {
-  type    = string
-  default = "10.1.0.0/16"
-}
-
-variable "proxy_setup" {
-  type    = string
-  default = "none"
-
-  validation {
-    condition     = contains(["none", "reverse", "forward"], var.proxy_setup)
-    error_message = "Allowed values for proxy_setup are:\n\"none\": no proxy. \"reverse\": using an azure firewall. \"forward\": using a simple squid proxy."
-  }
-}
-
-variable "subnet_aks_cidr" {
-  type    = string
-  default = "10.1.0.0/18"
-}
-
-variable "subnet_public_cidr" {
-  type    = string
-  default = "10.1.64.0/24"
-}
-
 variable "azure_china" {
   type        = bool
   default     = false
   description = "Set to true if deploying to Azure China Cloud"
-}
-
-variable "aks_subnet_private_endpoint_network_policies_enabled" {
-  type = bool
-  default = true
-  description = "Set to false when installing an AKS private cluster, https://learn.microsoft.com/en-us/azure/private-link/disable-private-endpoint-network-policy" 
 }
