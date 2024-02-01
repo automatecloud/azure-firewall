22d4dd3e11bd3841aac0801a5b428b88b6f5dc84
Andreas Wilke <andiwilke@gmail.com>
2024-02-01 19:43:14 +0100

    delete old module

--- old module/modules/reverse-proxy/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "proxy_public_ip" {
-  description = "Proxy server public IP"
-  value       = azurerm_public_ip.proxy_public_ip.ip_address
-}
-
-output "proxy_private_ip" {
-  description = "Proxy server private IP"
-  value       = azurerm_firewall.reverse_proxy.ip_configuration[0].private_ip_address
-}
